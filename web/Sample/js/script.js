b03a1dfb26eae82014e5ba35be6996bba31ea72e
beumseuk <dusgml2022@gmail.com>
2022-10-31 15:55:19 +0900

    first-commits

---
+++ web/Sample/js/script.js
@@ -0,0 +1,32 @@
+var slideIndex = 0;
+function slideAuto(){
+    $(".backimg>li").removeClass("topImg");
+    //.backimg안에 있는 모든 li에 접근 한 후 거기에 topImg클래스가 있다면 지워라
+    slideIndex++;
+    var isIdx = slideIndex % 3;
+    $(".backimg>li").eq(isIdx).addClass("topImg");
+}
+
+var slideCall = setInterval("slideAuto()", 1500);
+
+$(".headerWrap").hover(function(){
+    clearInterval(slideCall);
+},function(){
+    slideCall = setInterval("slideAuto()", 1500);
+});
+
+$(function(){
+    $("#gnb>li").hover(function(){
+        $(this).children(".sub").stop().fadeIn(500);
+    },function(){
+        $(this).children(".sub").stop().fadeOut(200);
+    });
+});
+
+
+$(function(){
+    $("#menu").click(function(){
+        $(this).toggleClass("aniMenu");
+        $("#gnb").fadeToggle(100);
+    });
+});
